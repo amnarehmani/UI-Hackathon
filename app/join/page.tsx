a48a973ffdecea86ce8102a2724a53215aadcc20
amnarehmani <jeonamnaaa@gmail.com>
2024-12-09 23:04:09 +0500

    main

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import { Checkbox } from "@/components/ui/checkbox"
 import Link from 'next/link'
 
-
 const join = () => {
   return (
     <div className="flex flex-col items-center justify-center p-4">
@@ -25,71 +24,76 @@ const join = () => {
         products, inspiration and community.
       </p>
       <div className="flex flex-col items-center justify-center">
-  <input
-    type="email"
-    required
-    placeholder="Email Address"
-    className="border p-2 w-[324px] h-[40px] rounded-[3px] bg-white text-[#8D8D8D] mb-4"
-  />
-  <input
-    type="password"
-    required
-    placeholder="Password"
-    className="border p-2 w-[324px] h-[40px] rounded-[3px] bg-white text-[#8D8D8D] mb-4"
-  />
-  <input
-    type="name"
-    required
-    placeholder="First Name"
-    className="border p-2 w-[324px] h-[40px] rounded-[3px] bg-white text-[#8D8D8D] mb-4"
-  />
-  <input
-    type="name"
-    required
-    placeholder="Last Name"
-    className="border p-2 w-[324px] h-[40px] rounded-[3px] bg-white text-[#8D8D8D] mb-4"
-  />
-  <input
-    type="name"
-    required
-    placeholder="Date of Birth"
-    className="border p-2 w-[324px] h-[40px] rounded-[3px] bg-white text-[#8D8D8D] mb-1"
-  />
- 
-  
-  <p className="text-center text-[#8D8D8D] font-[400] text-[11px] pb-3 ">Get a Nike Member Reward every year on your Birthday.</p>
+        <input
+          type="email"
+          required
+          placeholder="Email Address"
+          className="border p-2 w-[324px] h-[40px] rounded-[3px] bg-white text-[#8D8D8D] mb-4"
+        />
+        <input
+          type="password"
+          required
+          placeholder="Password"
+          className="border p-2 w-[324px] h-[40px] rounded-[3px] bg-white text-[#8D8D8D] mb-4"
+        />
+        <input
+          type="name"
+          required
+          placeholder="First Name"
+          className="border p-2 w-[324px] h-[40px] rounded-[3px] bg-white text-[#8D8D8D] mb-4"
+        />
+        <input
+          type="name"
+          required
+          placeholder="Last Name"
+          className="border p-2 w-[324px] h-[40px] rounded-[3px] bg-white text-[#8D8D8D] mb-4"
+        />
+        <input
+          type="name"
+          required
+          placeholder="Date of Birth"
+          className="border p-2 w-[324px] h-[40px] rounded-[3px] bg-white text-[#8D8D8D] mb-1"
+        />
 
-  <input
-    type="name"
-    required
-    placeholder="India"
-    className="border p-2 w-[324px] h-[40px] rounded-[3px] bg-white text-[#8D8D8D]  mb-4"
-  /> 
+        <p className="text-center text-[#8D8D8D] font-[400] text-[11px] pb-3">
+          Get a Nike Member Reward every year on your Birthday.
+        </p>
 
-  <div className="flex w-[324px] mb-4 gap-3">
-    <button className="w-1/2  border rounded-l-[3px] bg-white text-[#8D8D8D]">
-      Male
-    </button>
-    <button className="w-1/2 p-2 border rounded-r-[3px] bg-white text-[#8D8D8D]">
-      Female
-    </button>
-  </div>
+        <input
+          type="name"
+          required
+          placeholder="India"
+          className="border p-2 w-[324px] h-[40px] rounded-[3px] bg-white text-[#8D8D8D]  mb-4"
+        />
 
-  <div className='flex gap-2 items-center justify-center'>
-  <Checkbox />
-  <p className="text-[#8D8D8D] font-[400] text-[13px]">
-  Sign up for emails to get updates from Nike on <br/> products, offers and your Member benefits
-</p>
-<p className='text-[#8D8D8D] text-[13px] text-center font-[400]  mt-3'>
-  By creating an account, you agree to Nike&apos;s <span className='underline cursor-pointer'>Privacy <br /> Policy</span>  and <span className='underline cursor-pointer'>Terms of Use</span>
-</p>
+        <div className="flex w-[324px] mb-4 gap-3">
+          <button className="w-1/2  border rounded-l-[3px] bg-white text-[#8D8D8D]">
+            Male
+          </button>
+          <button className="w-1/2 p-2 border rounded-r-[3px] bg-white text-[#8D8D8D]">
+            Female
+          </button>
+        </div>
 
+        <div className='flex gap-2 items-center justify-center'>
+          <Checkbox />
+          <p className="text-[#8D8D8D] font-[400] text-[13px]">
+            Sign up for emails to get updates from Nike on <br /> products, offers and your Member benefits
+          </p>
+        </div>
 
-</div>
+        <p className='text-[#8D8D8D] text-[13px] text-center font-[400]  mt-3'>
+          By creating an account, you agree to Nike&apos;s <span className='underline cursor-pointer'>Privacy <br /> Policy</span>  and <span className='underline cursor-pointer'>Terms of Use</span>
+        </p>
+      </div>
 
-<button className='w-[324px] h-[40px] bg-black text-white bottom-1 mt-6 rounded-[3px]'>JOIN US</button>
+      <button className='w-[324px] h-[40px] bg-black text-white bottom-1 mt-6 rounded-[3px]'>
+        JOIN US
+      </button>
 
-<p className='text-center text-[#8D8D8D]  mt-4 font-[13px] '>Already a Member? <Link href="/signin" className='underline font-[500] text-black'>Sign In</Link>.</p>
+      <p className='text-center text-[#8D8D8D]  mt-4 font-[13px]'>
+        Already a Member? <Link href="/signin" className='underline font-[500] text-black'>Sign In</Link>.
+      </p>
     </div>
   )
 }
